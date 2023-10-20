cc666eed36a536efcc4767680b4d84919922a467
Al7assan <al7assan.r@gmail.com>
2023-10-20 03:00:50 +0300

    c 0.3

--- main.c
+++ main.c
@@ -1,28 +1,35 @@
 #include "monty.h"
 
+/**
+ * main - main fun
+ * @argc: string from getline
+ * @argv: delemitir
+ * Return: int
+*/
+
 int main(int argc, char *argv[])
 {
-    stack_t *stack = NULL;
-    int i;
-    char **lines;
-    char **tokens;
-    char *d = " \t\n";
+	stack_t *stack = NULL;
+	int i;
+	char **lines;
+	char **tokens;
+	char *d = " \t\n";
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    lines = open_file(argv);
-    for (i = 0; lines[i] != NULL ; i++)
-    {
-        tokens = get_token(lines[i],d);
-        if (tokens[0] == NULL)
-            continue;
-        _include(&stack, tokens, (i + 1));
-        _free(1,tokens);
-    }
-    _free(1, lines);
-    return (0);
+	lines = open_file(argv);
+	for (i = 0; lines[i] != NULL ; i++)
+	{
+		tokens = get_token(lines[i], d);
+		if (tokens[0] == NULL)
+			continue;
+		_include(&stack, tokens, (i + 1));
+		_free(1, tokens);
+	}
+	_free(1, lines);
+	return (0);
 }
