4164321cfcaa7cd82b2930481867a86f24ba0d70
Al7assan <al7assan.r@gmail.com>
2023-10-20 03:03:08 +0300

    c 0.4

--- _include.c
+++ _include.c
@@ -10,34 +10,34 @@
 
 int _include(stack_t **stack, char **s, int x)
 {
-    int y = 0;
-    int i;
-    instruction_t commands[] = {
+	int y = 0;
+	int i;
+	instruction_t commands[] = {
 		{"push", &push_s},
 		{"pall", &pall},
-        {"NULL", NULL}
+		{"NULL", NULL}
 	};
-    for(i = 0; commands[i].opcode != NULL; i++)
-    {
-        if(strcmp(commands[i].opcode, s[0]) == 0)
-        {
+	for (i = 0; commands[i].opcode != NULL; i++)
+	{
+		if (strcmp(commands[i].opcode, s[0]) == 0)
+		{
 
-            if (strcmp(s[0], "pall") == 0)
-            {
-                commands[i].f(stack, y);
-            }
-            else
-            {
-                y = atoi(s[1]);
-                commands[i].f(stack, y);
-            }
-            break;
-        }
-        else if(strcmp(commands[i].opcode, "NULL") == 0)
-        {
-            fprintf(stderr, "L%i: unknown instruction %s\n", x, s[0]);
-            exit(EXIT_FAILURE);
-        }
-    }
-    return (0);
+			if (strcmp(s[0], "pall") == 0)
+			{
+				commands[i].f(stack, y);
+			}
+			else
+			{
+				y = atoi(s[1]);
+				commands[i].f(stack, y);
+			}
+			break;
+		}
+		else if (strcmp(commands[i].opcode, "NULL") == 0)
+		{
+			fprintf(stderr, "L%i: unknown instruction %s\n", x, s[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (0);
 }
